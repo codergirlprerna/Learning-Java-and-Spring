29db67eb0b38b4aa7e3571879c2faed269f1988f
codergirlprerna <prernakhanna2602@gmail.com>
2025-04-02 12:45:20 +0530

    Intital commit

---
+++ codingWallah/src/main/java/com/example/codingWallah/CodingWallahApplication.java
@@ -0,0 +1,13 @@
+package com.example.codingWallah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingWallahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingWallahApplication.class, args);
+	}
+
+}
---
+++ codingWallah/src/test/java/com/example/codingWallah/CodingWallahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingWallah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingWallahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
